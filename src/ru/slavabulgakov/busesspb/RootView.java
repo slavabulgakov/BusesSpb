515d2a9bee6ae6c91df7678d614a121584981854
Slava Bulgakov <slavabulgakov@gmail.com>
2013-04-01 02:53:11 +0400

    - переделал анимацию закрытия бокового меню

--- src/ru/slavabulgakov/busesspb/RootView.java
+++ src/ru/slavabulgakov/busesspb/RootView.java
@@ -4,16 +4,23 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.TranslateAnimation;
+import android.view.animation.Interpolator;
 import android.widget.RelativeLayout;
+import android.widget.Scroller;
 
 public class RootView extends RelativeLayout {
 	
+	private Scroller _scroller;
 	private void _load(Context context, AttributeSet attrs) {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		inflater.inflate(R.layout.root_view, this, true);
+		_scroller = new Scroller(getContext(), new Interpolator() {
+			
+			@Override
+			public float getInterpolation(float input) {
+				return (float)Math.pow(input - 1, 5) + 1;
+			}
+		});
 	}
 
 	public RootView(Context context) {
@@ -38,12 +45,12 @@ public class RootView extends RelativeLayout {
 	private Boolean _hold = false;
 	private Boolean _opened = false;
 	private float _prevX = 0;
+	private float _lastDX = 0;
 	@Override
 	public boolean onInterceptTouchEvent(MotionEvent ev) {
 		RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams)this.getLayoutParams();
 		switch (ev.getAction()) {
 		case MotionEvent.ACTION_DOWN:
-			System.out.printf("ACTION_DOWN on x:%f, leftMargin:%d, x - m:%f", ev.getRawX(), lp.leftMargin, ev.getRawX() - lp.leftMargin);
 			if (ev.getRawX() - lp.leftMargin <= _dpToPx(30)) {
 				_hold = true;
 			} else if (ev.getRawX() - lp.leftMargin > _dpToPx(30) && _opened) {
@@ -53,20 +60,18 @@ public class RootView extends RelativeLayout {
 			break;
 			
 		case MotionEvent.ACTION_MOVE:
-			System.out.println(ev.getRawX());
 			float dX = ev.getRawX() - _prevX;
 			if (_hold) {
-				if (lp.leftMargin + dX > _dpToPx(200) && !_opened) {
-					_opened = true;
+				if (lp.leftMargin + dX > _dpToPx(200)) {
 					lp.leftMargin = _dpToPx(200);
 					lp.rightMargin = _dpToPx(-200);
-				} else if (lp.leftMargin + dX < 0 && _opened) {
-					_opened = false;
+				} else if (lp.leftMargin + dX < 0) {
 					lp.leftMargin = 0;
 					lp.rightMargin = 0;
 				} else {
 					lp.leftMargin += dX;
 					lp.rightMargin -= dX;
+					_lastDX = dX;
 					_prevX = ev.getRawX();
 				}
 				setLayoutParams(lp);
@@ -75,16 +80,19 @@ public class RootView extends RelativeLayout {
 			
 		case MotionEvent.ACTION_UP:
 		case MotionEvent.ACTION_CANCEL:
-			if (lp.leftMargin <= _dpToPx(30)) {
+			if (lp.leftMargin <= _dpToPx(10)) {
 				_opened = false;
 				lp.leftMargin = 0;
 				setLayoutParams(lp);
-			} else if (lp.leftMargin > _dpToPx(190)) {
-				_opened = false;
+			} else if (lp.leftMargin > _dpToPx(190) && _opened) {
+				_animateMove(0);
+			}
+			else if (lp.leftMargin > _dpToPx(190)) {
+				_opened = true;
 				lp.leftMargin = _dpToPx(200);
 				setLayoutParams(lp);
 			} else {
-				_animateMove();
+				_animateMove(0);
 			}
 			_hold = false;
 			break;
@@ -95,49 +103,78 @@ public class RootView extends RelativeLayout {
 		return super.onInterceptTouchEvent(ev); 
 	}
 	
-	private void _animateMove() {
-		final RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams)this.getLayoutParams();
-		int dX = 0;
-		if (lp.leftMargin < _dpToPx(100) && _opened) {
-			dX = -lp.leftMargin;
-		} else if (lp.leftMargin < _dpToPx(100) && !_opened) {
-			dX = 200 - lp.leftMargin;
-		} else {
-			if (_opened) {
-				dX = -lp.leftMargin;
-			} else {
-				dX = 200 - lp.leftMargin;
-			}
+	public void open() {
+		if (!_opened) {
+			_animateMove(1);
 		}
-		lp.leftMargin += dX;
-		lp.rightMargin -= dX;
-		TranslateAnimation ta = new TranslateAnimation(0, dX, 0, 0);
-		ta.setDuration(400);
-		ta.setAnimationListener(new AnimationListener() {
+	}
+	
+	public void close() {
+		if (_opened) {
+			_animateMove(-1);
+		}
+	}
+	
+	public void toggle() {
+		if (_opened) {
+			_animateMove(-1);
+		} else {
+			_animateMove(1);
+		}
+	}
+	
+	public Boolean isOpen() {
+		return _opened;
+	}
+	
+	private void _animateMove(int direction) {
+		final RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams)this.getLayoutParams();
+		if (direction > 0) {
+			_scroller.startScroll(lp.leftMargin, 0, _dpToPx(200) - lp.leftMargin, 0);
+		} else if (direction < 0) {
+			_scroller.startScroll(lp.leftMargin, 0, -lp.leftMargin, 0);
+		} else if (_lastDX > 0) {
+			_scroller.startScroll(lp.leftMargin, 0, _dpToPx(200) - lp.leftMargin, 0);
+		} else if (_lastDX < 0) {
+			_scroller.startScroll(lp.leftMargin, 0, -lp.leftMargin, 0);
+		}
+		
+		post(new Runnable() {
 			
 			@Override
-			public void onAnimationStart(Animation animation) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				setLayoutParams(lp);
-				if (lp.leftMargin < 100) {
-					_opened = false;
+			public void run() {
+				if (_scroller.isFinished()) {
+					if (lp.leftMargin < 100) {
+						lp.leftMargin = 0;
+						_opened = false;
+					} else {
+						lp.leftMargin = _dpToPx(200);
+						_opened = true;
+					}
+					setLayoutParams(lp);
+					return;
+				}
+				Boolean more = _scroller.computeScrollOffset();
+				int dX = 0;
+				int currentX = _scroller.getCurrX();
+				dX = currentX - (int)_prevX;
+				if (lp.leftMargin + dX > _dpToPx(200)) {
+					lp.leftMargin = _dpToPx(200);
+					lp.rightMargin = _dpToPx(-200);
+				} else if (lp.leftMargin + dX < 0) {
+					lp.leftMargin = 0;
+					lp.rightMargin = 0;
 				} else {
-					_opened = true;
+					lp.leftMargin += dX;
+					lp.rightMargin -= dX;
+					_prevX = currentX;
+				}
+				setLayoutParams(lp);
+				
+				if (more) {
+					postDelayed(this, 16);
 				}
 			}
 		});
-		this.startAnimation(ta);
 	}
-
 }
