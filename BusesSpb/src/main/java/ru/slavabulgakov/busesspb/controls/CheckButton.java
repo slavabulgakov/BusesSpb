94a5439fcb5fa3a289ffdbc446c9f62417e6b009
Slava Bulgakov <sbulgakov@devpocket.com>
2013-12-24 13:00:09 +0400

    - перевел проект на Android Studio (Gradle)

---
+++ BusesSpb/src/main/java/ru/slavabulgakov/busesspb/controls/CheckButton.java
@@ -0,0 +1,65 @@
+package ru.slavabulgakov.busesspb.controls;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import ru.slavabulgakov.busesspb.R;
+
+public class CheckButton extends LinearLayout implements OnClickListener {
+	boolean _checked;
+	LinearLayout _linearLayout;
+	ImageView _imageView;
+	OnClickListener _listener;
+	int _id;
+
+	public CheckButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CheckButton, 0, 0);
+		Drawable icon = a.getDrawable(R.styleable.CheckButton_android_src);
+		int iconBackground = a.getResourceId(R.styleable.CheckButton_iconBackground, R.drawable.ticket_bg_bus);
+		int background = a.getResourceId(R.styleable.CheckButton_bg, R.drawable.btn_selected_black);
+		_checked = a.getBoolean(R.styleable.CheckButton_checked, false);
+		_id = a.getInt(R.styleable.CheckButton_android_id, -1);
+		a.recycle();
+		
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.check_button, this, true);
+		
+		_linearLayout = (LinearLayout)getChildAt(0);
+		_linearLayout.setBackgroundResource(background);
+		_linearLayout.setOnClickListener(this);
+		
+		_imageView = (ImageView)findViewById(R.id.checkButtonIcon);
+		_imageView.setImageDrawable(icon);
+		_imageView.setBackgroundResource(iconBackground);
+	}
+
+	@Override
+	public void onClick(View v) {
+		setChecked(!_checked);
+		if (_listener != null) {
+			_listener.onClick(this);
+		}
+	}
+	
+	public void setOnClickListener(OnClickListener listener) {
+		_listener = listener;
+	}
+	
+	public boolean checked() {
+		return _checked;
+	}
+	
+	public void setChecked(boolean checked) {
+		_checked = checked;
+		_linearLayout.setSelected(_checked);
+	}
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-BusesSpb
-========
-
-import existing android project "google-play-services_lib" from "adt-bundle-mac-x86_64/sdk/extras/google/google_play_services/libproject" to workspace
